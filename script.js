786661999c07bf8b3d66f0fec61bc5c7116e6771
Jabir <220501@student.glu.nl>
2024-02-06 15:28:49 +0100

    added button styling

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ gsap.to(".lid", { rotationX: 180, duration: 0.5, onComplete: slideEnvelope });
 function slideEnvelope() {
   gsap.to(".envelope, .cover, .lid, .shadow", {
     y: "80vmin",
-    duration: 3,
+    duration: 1,
     delay: 1,
   });
 }
