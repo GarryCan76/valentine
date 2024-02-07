91de3e2f9d7fe2214a81526906b148bd08f57c60
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2024-02-07 10:27:59 +0100

    jaaabi

--- js/index.js
+++ js/index.js
@@ -2,7 +2,11 @@ let url = window.location.href;
 let index = url.indexOf('name=')
 
 
+
 let name = url.substring(index + 5)
+console.log(index)
+if (index === -1){
+    name = '';
+}
 let nameElement = document.getElementById('name');
 nameElement.innerText = name;
-console.log(name)
