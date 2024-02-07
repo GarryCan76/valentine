9373d1f35e5442a3787d34f9375246b2b051edb8
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2024-02-07 11:43:33 +0100

    jaaabi

--- js/index.js
+++ js/index.js
@@ -1,7 +1,44 @@
 let url = window.location.href;
 let index = url.indexOf('name=')
+let mousePos = false;
+let noBtn = document.getElementById('no_btn');
+let card = document.getElementById('card');
+
+document.onmousemove=function (e){
+    let bound = 40;
+    mousePos = {'x':e.clientX, 'y':e.clientY};
+    let offset = getOffset(noBtn)
+    let posChange = {'y':0};
+    // console.log(offset)
+    // console.log(mousePos)
 
 
+    if (offset.top - bound < mousePos.y && offset.top > mousePos.y){
+        console.log('top')
+        posChange.y += 20;
+    }else if (offset.top < mousePos.y && offset.top + bound > mousePos.y){
+        console.log('bottom')
+        posChange.y -= 20;
+    }
+    console.log(posChange)
+    // console.log(mousePos)
+    changePos(card, posChange)
+
+}
+
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left + window.scrollX,
+        top: rect.top + window.scrollY
+    };
+}
+
+function changePos(element, cardOffset){
+    console.log("oki " + element.style.top)
+    element.style.left = 0 +'px';
+    element.style.top = cardOffset.y  +'px'
+}
 
 let name = url.substring(index + 5)
 console.log(index)
